f73230630477cd5c79f3a0c347def506a909216c
aidinio <aidin.webs@gmail.com>
2025-06-13 20:59:06 +0330

    Fix responsive bugs

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -6,7 +6,7 @@ export default async function Navbar({ hidden }: { hidden?: boolean }) {
   return (
     <ul
       className={clsx(
-        `grow-[0] justify-self-start z-30 shadow-default md:shadow-none fixed bg-white md:bg-transparent h-fit rounded-full px-[2.4rem] py-[5px] md:py-0 md:px-0 md:rounded-none top-9 flex md:flex-col gap-8 md:gap-5 md:h-full items-start justify-center shrink-0 md:sticky`,
+        `grow-[0] justify-self-start z-30 shadow-default md:shadow-none fixed bg-white md:bg-transparent h-fit rounded-full px-[2.4rem] py-[5px] md:py-0 md:px-0 md:rounded-none top-9 flex md:flex-col gap-7 md:gap-5 md:h-full items-start justify-center shrink-0 md:sticky`,
         { "md:hidden static opacity-0 mt-9": hidden }
       )}
     >
--- src/ui/home/AboutCard.tsx
+++ src/ui/home/AboutCard.tsx
@@ -3,8 +3,8 @@ import PrimaryLink from "@/components/custom/link/PrimaryLink";
 
 export default function AboudCard() {
   return (
-    <div className="xl:size-[36rem] xl:w-[36rem] w-[80vw] max-w-[27rem] xl:max-w-[36rem]  bg-white rounded-[40px] md:rounded-[2.5rem] xl:rounded-[3.75rem] flex -mt-[7rem] md:mt-0 flex-col gap-[2rem] md:gap-[3rem] 2xl:gap-[1.875rem] items-center justify-between py-[2.75rem] md:py-[2rem] lg:py-[3rem] px-[2.5rem] md:px-[2.7rem] lg:px-[3rem] 2xl:px-[5rem] drop-shadow-[10px_10px_0_0_rgba(0,0,0,1)] shadow-default">
-      <h1 className="text-[3.75rem] md:text-[3rem] xl:text-[4.2rem] 2xl:text-[6rem] font-bold w-fit">
+    <div className="2xl:size-[36rem] xl:h-[fit] xl:w-[36rem] w-[80vw] max-w-[27rem] xl:max-w-[36rem]  bg-white rounded-[40px] md:rounded-[2rem] xl:rounded-[3.75rem] flex -mt-[7rem] md:mt-0 flex-col gap-[2rem] md:gap-[2rem] 2xl:gap-[1.875rem] items-center justify-between py-[2.75rem] md:py-[2rem] lg:py-[3rem] px-[2.5rem] md:px-[2rem] lg:px-[3rem] 2xl:px-[5rem] drop-shadow-[10px_10px_0_0_rgba(0,0,0,1)] shadow-default">
+      <h1 className="text-[3.75rem] md:text-[2.5rem] lg:text-[3rem] xl:text-[4.2rem] 2xl:text-[5.6rem] font-bold w-fit">
         {`I'm`}{" "}
         <GradientText>Aidin</GradientText>
       </h1>
@@ -18,7 +18,7 @@ export default function AboudCard() {
       <PrimaryLink href="/about">
         read more
         <div
-          className={`bg-[url(/arrow.svg)] bg-center bg-size-[cover] size-[1rem] md:size-[20px] bg-no-repeat inline-block`}
+          className={`bg-[url(/arrow.svg)] bg-center bg-size-[cover] size-[1rem] md:size-[1.25rem] bg-no-repeat inline-block`}
         />
       </PrimaryLink>
     </div>
