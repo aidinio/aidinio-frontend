f76d99d43952b26a4f8b20353a3ee875b50a5949
aidinio <aidin.webs@gmail.com>
2025-07-31 20:52:17 +0330

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,12 @@ const nextConfig: NextConfig = {
         port: "1337",
         pathname: "/**",
       },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
+        pathname: "/**",
+      },
     ],
   },
 };
