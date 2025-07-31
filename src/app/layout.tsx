b783ee7c1186c714d5f07353190aba75b4d22200
aidinio <aidin.webs@gmail.com>
2025-07-31 20:38:21 +0330

    Optimize background gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import AnimatedElements from "@/ui/AnimatedElements";
 import Navbar from "@/ui/Navbar";
 import FilterDefinitions from "@/components/custom/FilterDefinitions";
 
@@ -34,7 +33,6 @@ export default function RootLayout({
         <div className="bg-black absolute top-0 left-0 w-[100vw] h-[100vh]"></div>
         <div className="bg-white rounded-[2rem] sm:rounded-[3.5rem] absolute top-0 left-0 w-[100vw] h-[100vh] p-0 overflow-hidden">
           <div className="relative z-10 h-full overflow-auto">
-            {/* <div className="fixed z-[2] bg-white rounded-[100px] left-[0px] top-[0px] w-[100vw] h-[100vh] block filter-[url(#nnnoise-filter)] resize-none opacity-60" /> */}
             <div className="fixed top-0 left-0 w-[100vw] h-[100vh] z-[2]">
               <svg className="absolute top-0 left-0 w-full h-full">
                 <rect
@@ -48,9 +46,7 @@ export default function RootLayout({
               </svg>
             </div>
             <div className="blur-[50px] sm:blur-[150px] w-[100vw] h-[100vh] z-[1] fixed top-0 cat">
-              <div className="filter-[url(#goo)] overflow-hidden">
-                <AnimatedElements />
-              </div>
+              <div className="gradient-bg "></div>
             </div>
             <div className="relative py-3 sm:py-0 sm:px-10 box-border z-10 w-[100vw] h-[100vh] max-h-[100vh] overflow-hidden">
               <div className="flex flex-col sm:flex-row items-center-safe justify-center xl:justify-start h-full box-border max-h-[100vh] sm:gap-[1rem] xl:gap-[2rem] 2xl:gap-[3.75rem] overflow-hidden">
